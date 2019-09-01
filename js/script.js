6583c6a2eb3cb8615231b5754e5d625baf052713
MrSuji <45489863+MrSuji@users.noreply.github.com>
2019-09-01 09:08:27 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 document.querySelector('.fa-bars').addEventListener('click', function() {
   var element1 = document.querySelector('#particles-js');
   element1.classList.toggle('toggleParticle');
-
-  document.querySelector('.aboutCard').classList.toggle('activeCardToggle');
-});
-
-document.addEventListener('click', function(event) {
-  console.log(event.target);
 });
